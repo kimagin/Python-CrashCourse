94241d47f86ce3d08218b09887ff5dce5fb4e34e
kimagin <info@kimagination.net>
2023-07-19 10:12:11 +0200

    Python Loops

---
+++ loops.py
@@ -0,0 +1,57 @@
+#NOTE: A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string)
+
+people = ['John','Paul', 'Sara','Susan']
+
+'''
+Simple FOR loop
+'''
+print('\nSimple for loop:')
+
+for person in people:
+    print(f'Current Person: {person}')
+
+'''
+BREAK
+'''
+print('\nBreak:')
+
+for person in people:
+    if person == 'Sara':
+        break
+    print(f'Current Person: {person}')
+
+'''
+CONTINUE
+'''
+print('\nContinue:')
+
+for person in people:
+    if person == 'Sara':
+        continue
+    print(f'Current Person: {person}')
+
+'''
+RANGE
+'''
+print('\nRange:')
+
+for i in range(len(people)):
+    print(people[i])
+'''
+CUSTOM RANGE
+'''
+print('\nCustom Range:')
+for i in range(0,11):
+    print(f'Number:{i}')
+
+'''
+WHILE
+'''
+print('\nWhile:')
+count = 0
+while count <= 10:
+    print(f'Count:{count}')
+    count += 1
+
+
+
